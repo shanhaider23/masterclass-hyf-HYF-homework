e705844865d4a0daf8c6536e8a6577ed95b0cc05
śhanhaider23 <shan.haider@gmail.com>
2022-02-28 19:33:44 +0100

    week 1 homework submit

---
+++ week1/excercise-2/ex2.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string input = "Intellectualization";
+int GetVowelCount(string str)
+{
+int vowelCount = 0;
+    foreach (char letter in str)
+    {
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+        {
+            vowelCount++;
+        }
+    }
+    return vowelCount;
+}
+
+Console.WriteLine($"Number of vowels: {GetVowelCount(input)}"); 
---
+++ week1/excercise-3/ex3.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arr = new[] { 271, -3, 1, 14, -100, 13, 2, 1, -8, -59, -1852, 41, 5 };
+int[] resultArray = GetResult(arr);
+int[] GetResult(int[] arr)
+{
+    int positiveNumber = 1;
+    int negativeNumber = 0;
+    foreach (int n in arr)
+    {
+        if (n > 0)
+        {
+            positiveNumber =positiveNumber * n;
+
+        }
+        else
+        {
+            negativeNumber = negativeNumber + n;
+        }
+    }
+    return new[] { negativeNumber, positiveNumber };
+}
+Console.WriteLine($"Sum of negative numbers: {resultArray[0]}. Multiplication of positive numbers: {resultArray[1]}");
