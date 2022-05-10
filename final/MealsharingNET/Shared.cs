e5e2abdcce79d25d7b8ee3d8a21e1ab79b194c42
śhanhaider23 <shan.haider@gmail.com>
2022-05-10 19:27:46 +0200

    REVERT

--- final/MealsharingNET/Shared.cs
+++ final/MealsharingNET/Shared.cs
@@ -1,7 +1,7 @@
 
 public class Shared
 {
-    public static string ConnectionString = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MealSharingDb")) ?
+    public static string ConnectionString = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb")) ?
    "Server=localhost;Database=meal_application;Uid=root;Pwd=sadia572;Convert Zero Datetime=True" :
-   Environment.GetEnvironmentVariable("MealSharingDb");
+   Environment.GetEnvironmentVariable("MYSQLCONNSTR_MealSharingDb");
 }
